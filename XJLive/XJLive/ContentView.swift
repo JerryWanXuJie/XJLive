a3eeb3f940509f00028c4d24fe71f52639a40445
xujiewan <swing260018@163.com>
2020-04-19 21:46:35 +0800

    创建Xcode项目基于SwiftUI

---
+++ XJLive/XJLive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XJLive
+//
+//  Created by 万旭杰 on 2020/4/19.
+//  Copyright © 2020 万旭杰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XJLive/XJLiveTests/XJLiveTests.swift
@@ -0,0 +1,34 @@
+//
+//  XJLiveTests.swift
+//  XJLiveTests
+//
+//  Created by 万旭杰 on 2020/4/19.
+//  Copyright © 2020 万旭杰. All rights reserved.
+//
+
+import XCTest
+@testable import XJLive
+
+class XJLiveTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
